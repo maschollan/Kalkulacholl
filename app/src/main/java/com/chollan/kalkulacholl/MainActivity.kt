6c8d18dc20bc97e9d8d147c9e4ebb4e689dc0d45
Mac Chollan <kholan.childs404@gmail.com>
2021-11-01 22:40:29 +0700

    Create simple kalkulacholl app

--- app/src/main/java/com/chollan/kalkulacholl/MainActivity.kt
+++ app/src/main/java/com/chollan/kalkulacholl/MainActivity.kt
@@ -2,10 +2,65 @@ package com.chollan.kalkulacholl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnTambah : Button
+    private lateinit var btnKurang : Button
+    private lateinit var btnKali : Button
+    private lateinit var btnBagi : Button
+    private lateinit var btnClear : Button
+    private lateinit var angka1 : EditText
+    private lateinit var angka2 : EditText
+    private lateinit var textHasil : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnTambah = findViewById(R.id.btnTambah)
+        btnKurang = findViewById(R.id.btnKurang)
+        btnKali = findViewById(R.id.btnKali)
+        btnBagi = findViewById(R.id.btnBagi)
+        btnClear = findViewById(R.id.btnClear)
+        angka1 = findViewById(R.id.angka1)
+        angka2 = findViewById(R.id.angka2)
+        textHasil = findViewById(R.id.textHasil)
+
+        btnTambah.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            val inputAngka1 = angka1.text.toString().trim()
+            val inputAngka2 = angka2.text.toString().trim()
+
+            when (v.id) {
+                R.id.btnTambah -> {
+                    val jumlah = inputAngka1.toDouble() + inputAngka2.toDouble()
+                    textHasil.text = jumlah.toString()
+                }
+                R.id.btnKurang -> {
+                    val jumlah = inputAngka1.toDouble() - inputAngka2.toDouble()
+                    textHasil.text = jumlah.toString()
+                }
+                R.id.btnKali -> {
+                    val jumlah = inputAngka1.toDouble() * inputAngka2.toDouble()
+                    textHasil.text = jumlah.toString()
+                }
+                R.id.btnBagi -> {
+                    val jumlah = inputAngka1.toDouble() / inputAngka2.toDouble()
+                    textHasil.text = jumlah.toString()
+                }
+                R.id.btnClear -> {
+                    angka1.text.clear()
+                    angka2.text.clear()
+                    textHasil.text = "0"
+                }
+            }
+        }
     }
 }
